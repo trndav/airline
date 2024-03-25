edbcf2c1083df1c134f9fa0c34d784e35d9f7bcb
trndav <farsiteguide@gmail.com>
2024-03-25 11:59:46 +0100

    Wrong valid flight function

--- airline/urls.py
+++ airline/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from flights import views as flights_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("flights/", include("flights.urls")),
-    path("users/", include("users.urls"))
+    path("users/", include("users.urls")),
+    path("", flights_views.index, name="index"),
 ]
--- flights/views.py
+++ flights/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render, get_object_or_404
 from .models import Flight, Passenger
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from .models import Flight
 
 def index(request):
-    return render(request, "flights/index.html", {
-        "flights": Flight.objects.all()
-    })
+    flights = Flight.objects.all()
+    return render(request, "flights/index.html", {"flights": flights})
+    # return render(request, "flights/index.html", {
+    #     "flights": Flight.objects.all()
+    # })
 
 def flight(request, flight_id):
     # flight = Flight.objects.get(pk=flight_id)
